e968ca4297aecc497f9993453769db7e9a14cbe7
Oleg Sobol <aleh.yatskavets@gmail.com>
2024-10-07 03:10:06 +0300

    Удаление всех папок __pycache__ из репозитория

--- project_automatic/endpoints/general_endpoints.py
+++
@@ -1,5 +0,0 @@
-class Endpoint:
-    url = "http://167.172.172.115:52355/meme"
-    response = None
-    headers = {'Content-Type': 'application/json',
-               'Authorization': 'eboKiFqb3trLVWg'}
